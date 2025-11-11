b3511ff8f016032530c4f9b449c8052863c4798e
Igor K. <adminfos@gmail.com>
2025-11-11 20:44:57 +0300

    ---

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.urls import path
-# from main.views import signup, logout, login
-
-#
-# urlpatterns = [
-#     path('api/v1/user/login/', login,),
-#     path('api/v1/user/logout/', logout,),
-#     path('api/v1/user/signup/', signup,),
-# ]
--- orders/urls.py
+++
@@ -1,9 +0,0 @@
-# orders/urls.py
-
-from rest_framework.routers import DefaultRouter
-from orders.views import OrderViewSet
-
-router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='order')
-
-urlpatterns = router.urls
